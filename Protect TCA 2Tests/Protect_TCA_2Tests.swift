bdbd5b77517b0105a8db2c7f7c9a49b0063eb17b
AkitoshiAsakura <akitoshi.a@icloud.com>
2016-02-14 19:45:21 +0900

    Initial Commit

---
+++ Protect TCA 2Tests/Protect_TCA_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Protect_TCA_2Tests.swift
+//  Protect TCA 2Tests
+//
+//  Created by 朝倉明俊 on H28/02/14.
+//  Copyright © 平成28年 朝倉明俊. All rights reserved.
+//
+
+import XCTest
+@testable import Protect_TCA_2
+
+class Protect_TCA_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
